4a771a5206bf36856e56d89d1f67407f154be593
roney ruiz <roneyruizrojas@gmail.com>
2026-03-20 22:11:41 -0600

    chore: create gql tagged sample

--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,13 @@
-export function greet(name: string): string {
-  return `Hello, ${name}!`;
-};
+import { gql } from "./gql/tagged";
+
+type TestData = { hello: string };
+type TestVars = { name: string };
+
+const TEST_QUERY = gql<TestData, TestVars>`
+  query Test($name: String!) {
+    hello
+  }
+`;
+
+/* const userId = "42";
+const query = gql`query { user(id: ${userId}) { name } }`; */
