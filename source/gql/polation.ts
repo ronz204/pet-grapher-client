f57b2a794018db5c23bcf4b76b8cebd02d0c6faf
roney ruiz <roneyruizrojas@gmail.com>
2026-03-20 22:05:11 -0600

    feat: complete gql interpolation security

---
+++ source/gql/polation.ts
@@ -0,0 +1,12 @@
+export function interpolation(...values: unknown[]): void {
+  if (values.length > 0) {
+    throw new Error(
+      'gql tag does not support interpolation.\n' +
+      'Use GraphQL variables instead:\n\n' +
+      '  // ❌ BAD:\n' +
+      '  gql`query { user(id: ${id}) }`\n\n' +
+      '  // ✅ GOOD:\n' +
+      '  gql`query($id: ID!) { user(id: $id) }`'
+    );
+  };
+};
